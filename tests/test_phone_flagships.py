fc5d0ae67f2b79380fbf63d241df033f550d9971
samannazsheyda-commits <samannaz.sheyda@gmail.com>
2026-09-06 15:53:31 +0330

    test: parse mobile.ir seller row layout

--- tests/test_phone_flagships.py
+++ tests/test_phone_flagships.py
@@ -12,14 +12,14 @@ def test_parse_flagships_picks_latest_series_and_lowest_listed_price():
     html = '''
     <table>
       <tr><td>Apple iPhone 16 Pro Max</td><td>392,000,000 تومان</td></tr>
-      <tr><td>Apple iPhone 17 Pro Max</td><td>489,900,000 تومان</td></tr>
-      <tr><td>Apple iPhone 17 Pro Max</td><td>273,980,000 تومان</td></tr>
+      <tr><td>مشاهده فروشندگان</td><td>فروشگاه الف</td><td>تهران</td><td>امروز</td><td>Apple iPhone 17 Pro Max</td><td>489,900,000</td><td>نمودار قیمت</td></tr>
+      <tr><td>مشاهده فروشندگان</td><td>فروشگاه ب</td><td>اصفهان</td><td>امروز</td><td>Apple iPhone 17 Pro Max</td><td>273,980,000</td><td>نمودار قیمت</td></tr>
       <tr><td>Samsung Galaxy S25 Ultra</td><td>210,000,000 تومان</td></tr>
-      <tr><td>Samsung Galaxy S26 Ultra</td><td>340,000,000 تومان</td></tr>
-      <tr><td>Samsung Galaxy S26 Ultra</td><td>232,980,000 تومان</td></tr>
+      <tr><td>فروشگاه</td><td>تهران</td><td>امروز</td><td>Samsung Galaxy S26 Ultra</td><td>340,000,000</td></tr>
+      <tr><td>فروشگاه</td><td>شیراز</td><td>امروز</td><td>Samsung Galaxy S26 Ultra</td><td>232,980,000</td></tr>
       <tr><td>Xiaomi 16 Ultra</td><td>190,000,000 تومان</td></tr>
-      <tr><td>Xiaomi 17 Ultra</td><td>280,000,000 تومان</td></tr>
-      <tr><td>Google Pixel 11 Pro XL</td><td>205,000,000 تومان</td></tr>
+      <tr><td>فروشگاه</td><td>امروز</td><td>Xiaomi 17 Ultra</td><td>280,000,000</td></tr>
+      <tr><td>فروشگاه</td><td>امروز</td><td>Google Pixel 11 Pro XL</td><td>205,000,000</td></tr>
     </table>
     '''
     rows = parse_flagship_phone_prices(html)
