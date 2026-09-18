03f3ba88d0331efef02e505ca75e30ce06329e10
samannazsheyda-commits <samannaz.sheyda@gmail.com>
2026-09-18 17:56:16 +0330

    feat: register V4 editorial action routes

--- panel/wsgi.py
+++ panel/wsgi.py
@@ -9,6 +9,7 @@ from .live_api import bp as live_api_bp
 from .newsroom_api import bp as newsroom_api_bp
 from .source_manager import bp as source_manager_bp
 from .v4 import bp as panel_v4_bp
+from .v4_actions import bp as panel_v4_actions_bp
 from .weather_preview import bp as weather_preview_bp
 
 
@@ -29,4 +30,5 @@ app.register_blueprint(live_api_bp)
 app.register_blueprint(newsroom_api_bp)
 app.register_blueprint(source_manager_bp)
 app.register_blueprint(panel_v4_bp)
+app.register_blueprint(panel_v4_actions_bp)
 app.register_blueprint(weather_preview_bp)
