0d0206ad94727c675ceff8f3b601c3ad52970914
samannazsheyda-commits <samannaz.sheyda@gmail.com>
2026-09-19 00:03:53 +0330

    feat: register Luna Operator API

--- panel/wsgi.py
+++ panel/wsgi.py
@@ -7,6 +7,7 @@ from .app import create_app
 from .command_center import bp as command_center_bp
 from .live_api import bp as live_api_bp
 from .luna_assistant import bp as luna_assistant_bp
+from .luna_operator_api import bp as luna_operator_bp
 from .newsroom_api import bp as newsroom_api_bp
 from .source_manager import bp as source_manager_bp
 from .v4 import bp as panel_v4_bp
@@ -36,4 +37,5 @@ app.register_blueprint(newsroom_api_bp)
 app.register_blueprint(source_manager_bp)
 app.register_blueprint(panel_v4_bp)
 app.register_blueprint(luna_assistant_bp)
+app.register_blueprint(luna_operator_bp)
 app.register_blueprint(weather_preview_bp)
